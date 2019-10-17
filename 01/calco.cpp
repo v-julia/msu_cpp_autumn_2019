4baf5913efbfbf578ba6673a8f43b00331b58455
vjulia <vjulia94@gmail.com>
2019-10-17 07:53:00 +0300

    updated

--- 01/calco.cpp
+++ 01/calco.cpp
@@ -4,7 +4,6 @@
 
 enum token_value
 {
-    NUMBER,
     END,
     PLUS='+',
     MINUS='-',
@@ -12,68 +11,91 @@ enum token_value
     DIV='/',
 };
 
-std::stringstream cin;           // входной поток символов в вычисляемом выражении из строки агрументов
-token_value       curr_tok;      // текущий токен (число,знак операции или признак окончания)
 
-void  check_input_args(int argc, char* argv[]);
-int   term();
-int   expr();
-int   get_number();
-void  get_oper();
+void  check_input_args(int argc, char* argv[], std::stringstream& sexpr, int& numerr);
+int   term(std::stringstream& sexpr, int& numerr, token_value& curr_tok);
+int   expr(std::stringstream& sexpr, int& numerr);
+int   get_number(std::stringstream& sexpr, int& numerr, token_value& curr_tok);
+void  get_oper(std::stringstream& sexpr, int& numerr, token_value& curr_tok);
 
 
 
 int main(int argc, char* argv[])
 {
-    check_input_args(argc, argv); // получить входной поток символов cin из командной строки
+    int numerr=0;
+    std::stringstream sexpr;  // входной поток символов в вычисляемом выражении из строки агрументов
 
-    std::cout << expr();          // вычислить значение выражения
-    return 0;
+    check_input_args(argc, argv, sexpr, numerr);      // получить входной поток символов из командной строки
+
+    if ( numerr == 0 ) {
+        int res = expr(sexpr, numerr);             // вычислить значение выражения
+        if ( numerr == 0 ) {
+            std::cout << res;
+            }
+    }
+
+    return numerr;
 }
 
 // проверка входных аргументов
-void check_input_args(int argc, char* argv[])
+void check_input_args(int argc, char* argv[], std::stringstream& sexpr, int& numerr)
 {
     if ( argc != 2 ) {
-        std::cout << "input error - no expression!";
-        exit(1);
+        std::cerr << "input error - arg count != 1!";
+        numerr=1;
+        return;
     }
-    cin = std::stringstream(argv[1]);
+    sexpr = std::stringstream(argv[1]);
 }
 
 // обработка всех операций + или -
-int expr()
+int expr(std::stringstream& sexpr, int& numerr)
 {
-    int left = term(); // term выполнит серию подряд идущих операций * или /
-    for ( ;;)
+    token_value curr_tok=END; // текущий токен (знак операции или признак окончания)
+
+    int left = term(sexpr, numerr, curr_tok); // term выполнит серию подряд идущих операций * или /
+    if ( numerr != 0 ) return 0;
+
+    for ( ;;) {
         switch ( curr_tok ) { // значение curr_tok уже было получено в term() и оно может быть только + или -
         case PLUS:
-            left += term(); // случай '+'
+            left += term(sexpr, numerr, curr_tok); // случай '+'
             break;
         case MINUS:
-            left -= term(); // случай '-'
+            left -= term(sexpr, numerr, curr_tok); // случай '-'
             break;
         default:
             return left;
         }
+        if ( numerr != 0 ) return 0;
+    }
 }
 
 // обработка блока из операций * или /
-int term()
+int term(std::stringstream& sexpr, int& numerr, token_value& curr_tok)
 {
-    int left = get_number();
+    int left = get_number(sexpr, numerr, curr_tok);
+    if ( numerr != 0 ) return 0;
+
     for ( ;;) {
-        get_oper(); // запишет в глобальную переменную curr_tok вид прочитанной операции
+
+        get_oper(sexpr, numerr, curr_tok); // запишет в переменную curr_tok вид прочитанной операции
+        if ( numerr != 0 ) return 0;
+
         switch ( curr_tok ) {
         case MUL: // '*'
-            left *= get_number();
+            left *= get_number(sexpr, numerr, curr_tok);
+            if ( numerr != 0 ) return 0;
             break;
         case DIV: //  '/'
         {
-            int d = get_number();
+            int d = get_number(sexpr, numerr, curr_tok);
+            if ( numerr != 0 ) return 0;
             if ( d == 0 ) {
-                std::cout << "divide by zero in term()!";
-                exit(5);
+                std::cerr << "divide by zero in term()!";
+                numerr=5;
+                curr_tok=END;
+                return 0;
             }
             left /= d;
             break;
@@ -85,47 +107,50 @@ int term()
 }
 
 // считывает числа
-int get_number()
+int get_number(std::stringstream& sexpr, int& numerr, token_value& curr_tok)
 {
     char ch;
     int num;
 
     do { // пропускает пробелы после знака операции
-        if ( !cin.get(ch) ) {
-            std::cout << "invalid arg in get_number()!";
-            exit(2);
+        if ( !sexpr.get(ch) ) {
+            std::cerr << "invalid arg in get_number()!";
+            numerr=2;
+            curr_tok=END;
+            return 0;
             // если после знака операции нет числа - то ошибка
         }
     } while ( isspace(ch) );
 
     if ( isdigit(ch) ) {
-        cin.putback(ch);
-        cin >> num;
-        curr_tok=NUMBER; // в принципе обозначать curr_tok, когда он NUMBER для этого примера необязательно
+        sexpr.putback(ch);
+        sexpr >> num;
         return num;
     }
     else if ( ch == '-' ) { // унарный минус
-        return -get_number();
+        return -get_number(sexpr, numerr, curr_tok);
     }
     else {
-        std::cout << "invalid token in get_number()!";
-        exit(3);
+        std::cerr << "invalid token in get_number()!";
+        numerr=3;
+        curr_tok=END;
+        return 0;
         // если после знака операции не число, то тоже ошибка
     }
 }
 
 // считывает арифметические операции
-void get_oper()
+void get_oper(std::stringstream& sexpr, int& numerr, token_value& curr_tok)
 {
     char ch;
     do { // пропускает пробелы после числа
-        if ( !cin.get(ch) ) {
+        if ( !sexpr.get(ch) ) {
             curr_tok = END;
             return;
             // здесь после числа может не быть операции, поэтому - признак окончания расчетов
         }
     } while ( isspace(ch) );
-    
+
     switch ( ch ) {
     case '*':
     case '/':
@@ -134,8 +159,23 @@ void get_oper()
         curr_tok=token_value(ch);
         break;
     default:
-        std::cout << "invalid operation in get_oper()!";
-        exit(4);
+        std::cerr << "invalid operation in get_oper()!";
+        numerr=4;
+        curr_tok=END;
+        return;
         // здесь тоже - после числа должен быть знак операции, иначе - выход с ошибкой
     }
 }
+
+
+// некоторые примеры
+// 1) ./calco "    25/5*6+7 +-   3   /1+20*3*4  /  3   /   4      -1"  =  53 
+// 2) ./calco "  35 / 5 * 3-1 +-10*  9/3/   15      -66/11/   2/3   "  =  17 
+// с ошибками
+// 1)  ./calco = input error - arg count != 1!, retcode=1
+// 2)  ./calco "1+  2+" = invalid arg in get_number()!, retcode=2
+// 3)  ./calco "1+  2+a" = invalid token in get_number()!, retcode=3
+// 4)  ./calco "2*  2 2 + 2"  = invalid operation in get_oper()!, retcode=4
+// 5)  ./calco "2*  2/ 2 + 2/0+5" = invalid token in get_number()!, retcode=5
+
+
--- 01/test.py
+++ 01/test.py
@@ -5,8 +5,8 @@ encoding = 'utf-8'
 def check1(expr, result, rcode):
     cm = './calco ' + expr
     out =  subprocess.run(cm, stdout = subprocess.PIPE, shell=True)
-    #if out.returncode == 0
-    
+
+    #print(out)
     if out.returncode == rcode and result == str(out.stdout, encoding):
         print('{} = {}; passed'.format(expr, result))
     else:
@@ -19,15 +19,16 @@ check1('"2 + 2*2"', '6', 0)
 check1('"2/2 + 2 - 650/34"', '-16', 0)
 check1('"-1 * 2 + 646/34 - -1"', '18', 0)
 
-check1('', 'input error - no expression!', 1)
+check1('', '', 1)
 
-check1('"24*3 + "', 'invalid arg in get_number()!', 2)
+check1('"24*3 + "', '', 2)
 
-check1('"34 + * 2"', 'invalid token in get_number()!', 3)
-check1('"34 + 45/i"', 'invalid token in get_number()!', 3)
+check1('"34 + * 2"', '', 3)
+check1('"34 + 45/i"', '', 3)
 
 
-check1('"11*2 + 35 ! 7"', 'invalid operation in get_oper()!', 4)
-check1('"2 ! -2"', 'invalid operation in get_oper()!', 4)
+check1('"11*2 + 35 ! 7"', '', 4)
+check1('"2 ! -2"', '', 4)
 
-check1('"34/0 + 34 -2*2"', 'divide by zero in term()!', 5)
+check1('"34/0 + 34 -2*2"', '', 5)
+
