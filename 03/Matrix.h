980aec0fb442c8ef25250032f2cbb102228c9fc5
v-julia <vjulia94@gmail.com>
2019-11-04 01:14:43 +0300

    fixed

--- 03/Matrix.h
+++ 03/Matrix.h
@@ -6,99 +6,108 @@
 class Matrix
 {
 private:
+    // класс для одиночной строки
     class c_row
     {
     public:
         c_row(size_t cols)
         {
-            if ( cols < 1 ) throw std::out_of_range("");
-            row=static_cast<int*>( std::malloc(cols * sizeof(int)) );
+            row=new int[cols];
             col_count=cols;
-
         };
         ~c_row()
         {
-            free(row);
+           delete[] row;
         };
-        int& operator[](size_t i)
+        int& operator[](size_t i) const
         {
-            if ( i<1 || i> col_count ) throw std::out_of_range("");
-            return *( row + i - 1 );
+            if ( i < 0 || i >= col_count ) throw std::out_of_range("");
+            return row[i];
         }
         size_t getCols() { return col_count; }
 
     private:
         int* row;
-        int col_count;
+        size_t col_count;
     };
+
+        // класс для одиночной строки
+private:
+    // данные класса Matrix
+    c_row* matr;      // указатель на массив из строк
+    size_t row_count; // количество строк
+
 public:
     Matrix(size_t rows, size_t cols)
     {
-        if ( rows < 1 || cols < 1 ) throw std::out_of_range("");
-
-        matr=static_cast<c_row**>( std::malloc(rows * sizeof(c_row*)) );
-        row_count=rows;
-        for ( int i=0; i < row_count; ++i ) {
-            *( matr + i )=new c_row(cols);
+        // применяется placement new
+        matr=static_cast<c_row*>( operator new[](rows * sizeof(c_row)) );
+        for ( size_t i = 0; i < rows; ++i ) {
+            new ( matr + i ) c_row(cols);
         }
-
+        row_count=rows;
     }
     ~Matrix()
     {
-        for ( int i=0; i < row_count; ++i ) {
-            free(*( matr + i ));
+        // деструктор: освобождение памяти
+        for ( size_t i = 0; i < row_count; ++i ) {
+            matr[i].~c_row(); // вначале освободить память строк
         }
+        operator delete[](matr); // последний - освободить указатели на строки
 
     }
 
     size_t getRows() const { return row_count; }
-    size_t getCols() const { return ( *matr )->getCols(); }
-
-    c_row& operator[](size_t i)
+    size_t getCols() const
     {
-        if ( i<1 || i> row_count ) throw std::out_of_range("");
-        return **( matr + i - 1 );
+        if ( row_count > 0 ) {
+            return matr[0].getCols();
+        }
+        else {
+            return 0;
+        }
     }
-    
+    c_row& operator[](size_t i) const
+    {
+        if (i<0 || i >= row_count ) throw std::out_of_range("");
+        return matr[i];
+    }
+
     Matrix& operator*=(const int& c)
     {
         size_t cols=getCols();
-        for ( int j=1; j <= row_count; ++j ) {
-            for ( int i=1; i <= cols; ++i ) {
-                ( *this )[i][j]*=c;
+        for ( size_t j=0; j < row_count; ++j ) {
+            for ( size_t i=0; i < cols; ++i ) {
+                matr[i][j]*=c;
             }
         }
     }
     Matrix& operator+=(const int& c)
     {
         size_t cols=getCols();
-        for ( int j=1; j <= row_count; ++j ) {
-            for ( int i=1; i <= cols; ++i ) {
-                ( *this )[i][j]+=c;
+        for ( size_t j=0; j < row_count; ++j ) {
+            for ( size_t i=0; i < cols; ++i ) {
+                matr[i][j]*=c;
             }
         }
     }
-    bool operator!=( Matrix& otherM) 
+    bool operator!=(const Matrix& otherM) const
     {
         if ( this == &otherM ) return true;
         if ( row_count != otherM.getRows() ) return true;
         size_t cols=getCols();
-        if( cols!= otherM.getCols() ) return true;
-        for ( int i=1; i <= row_count; ++i ) {
-            for ( int j=1; j <= cols; ++j ) {
-                if( ( *this )[i][j] != otherM[i][j]) return true;
+        if ( cols != otherM.getCols() ) return true;
+        for ( size_t i=0; i < row_count; ++i ) {
+            for ( size_t j=0; j < cols; ++j ) {
+                if ( matr[i][j] != otherM[i][j] ) return true;
             }
         }
         return false;
     }
-    bool operator==(Matrix& otherM)
+    bool operator==(const Matrix& otherM) const
     {
         return !( ( *this ) != otherM );
     }
-
-private:
-    c_row** matr;
-    int row_count;
 
 };
 
--- 03/main.cpp
+++ 03/main.cpp
@@ -1,11 +1,14 @@
 ﻿
-#include <iostream>
 #include "Matrix.h"
+
+#include <iostream>
+#include <iomanip>
+
 
 int main()
 {
-    size_t rows=2;
-    size_t cols=3;
+    size_t rows=13;
+    size_t cols=6;
     Matrix mt(rows, cols);
     Matrix mt_eq(rows, cols);
     Matrix mt_ne(rows, cols);
@@ -14,20 +17,21 @@ int main()
     size_t gcols=mt.getCols();
     std::cout << "mt.getRows=" << grows << "; mt.getCols=" << gcols << ";\n";
 
-    int k=1;
-    for ( size_t i=1; i <= rows; ++i ) {
-        for ( size_t j=1; j <= cols; ++j ) {
+    int k=0;
+    for ( size_t i=0; i < rows; ++i ) {
+        for ( size_t j=0; j < cols; ++j ) {
             mt[i][j]=k;
             k+=1;
         }
     }
+    
 
     std::cout << "\n";
     std::cout << "Matrix mt:\n";
-    for ( size_t i=1; i <= rows; ++i ) {
-        for ( size_t j=1; j <= cols; ++j ) {
+    for ( size_t i=0; i < rows; ++i ) {
+        for ( size_t j=0; j < cols; ++j ) {
             int u=mt[i][j];
-            std::cout << "mt[" << i << "][" << j <<"]=" << u << " ";
+            std::cout << std::setw(6) << "mt[" << std::setw(2) << i << "][" << std::setw(2) << j << "]=" << std::setw(3) << u << " ";
         }
         std::cout << "\n";
 
@@ -35,8 +39,8 @@ int main()
     std::cout << "\n";
 
     int c=10;
-    for ( size_t i=1; i <= rows; ++i ) {
-        for ( size_t j=1; j <= cols; ++j ) {
+    for ( size_t i=0; i < rows; ++i ) {
+        for ( size_t j=0; j < cols; ++j ) {
             mt[i][j]*=c;
         }
     }
@@ -44,39 +48,39 @@ int main()
 
     std::cout << "\n";
     std::cout << "Matrix mt*(C="<< c << "):\n";
-    for ( size_t i=1; i <= rows; ++i ) {
-        for ( size_t j=1; j <= cols; ++j ) {
+    for ( size_t i=0; i < rows; ++i ) {
+        for ( size_t j=0; j < cols; ++j ) {
             int u=mt[i][j];
-            std::cout << "mt[" << i << "][" << j << "]=" << u << " ";
+            std::cout << std::setw(6) << "mt[" << std::setw(2) << i << "][" << std::setw(2) << j << "]=" << std::setw(3) << u << " ";
         }
         std::cout << "\n";
 
     }
     std::cout << "\n";
 
-    for ( size_t i=1; i <= rows; ++i ) {
-        for ( size_t j=1; j <= cols; ++j ) {
+    for ( size_t i=0; i < rows; ++i ) {
+        for ( size_t j=0; j < cols; ++j ) {
             mt_eq[i][j]= mt[i][j];
             mt_ne[i][j] = -5;
         }
     }
     std::cout << "\n";
-    std::cout << "Matrix equal to mt, mt_eq:\n";
-    for ( size_t i=1; i <= rows; ++i ) {
-        for ( size_t j=1; j <= cols; ++j ) {
+    std::cout << "Matrix mt_eq:\n";
+    for ( size_t i=0; i < rows; ++i ) {
+        for ( size_t j=0; j < cols; ++j ) {
             int u=mt_eq[i][j];
-            std::cout << "mt_eq[" << i << "][" << j << "]=" << u << " ";
+            std::cout << std::setw(6) << "mt_eq[" << std::setw(2) << i << "][" << std::setw(2) << j << "]=" << std::setw(3) << u << " ";
         }
         std::cout << "\n";
 
     }
     std::cout << "\n";
     std::cout << "\n";
-    std::cout << "Matrix not equal to mt, mt_ne:\n";
-    for ( size_t i=1; i <= rows; ++i ) {
-        for ( size_t j=1; j <= cols; ++j ) {
+    std::cout << "Matrix mt_ne:\n";
+    for ( size_t i=0; i < rows; ++i ) {
+        for ( size_t j=0; j < cols; ++j ) {
             int u=mt_ne[i][j];
-            std::cout << "mt_ne[" << i << "][" << j << "]=" << u << " ";
+            std::cout << std::setw(6) << "mt_ne[" << std::setw(2) << i << "][" << std::setw(2) << j << "]=" << std::setw(3) << u << " ";
         }
         std::cout << "\n";
 
@@ -88,12 +92,26 @@ int main()
     bool test_eq_mt_vs_mt_ne = ( mt == mt_ne );
     bool test_ne_mt_vs_mt_ne = ( mt != mt_ne );
 
-    std::cout << "(mt == mt_eq) = " << ( test_eq_mt_vs_mt_eq ? "true" : "false" ) << "\n";
-    std::cout << "(mt != mt_eq) = " << ( test_ne_mt_vs_mt_eq ? "true" : "false" ) << "\n";
-    std::cout << "(mt == mt_ne) = " << ( test_eq_mt_vs_mt_ne ? "true" : "false" ) << "\n";
-    std::cout << "(mt != mt_ne) = " << ( test_ne_mt_vs_mt_ne ? "true" : "false" ) << "\n";
+    std::cout.setf(std::ios::boolalpha);
+    std::cout << "(mt == mt_eq) = " << test_eq_mt_vs_mt_eq << "\n";
+    std::cout << "(mt != mt_eq) = " << test_ne_mt_vs_mt_eq << "\n";
+    std::cout << "(mt == mt_ne) = " << test_eq_mt_vs_mt_ne << "\n";
+    std::cout << "(mt != mt_ne) = " << test_ne_mt_vs_mt_ne << "\n";
     std::cout << "\n";
 
+    // тест на исключения
+    try {
+        mt[20][ 1]=0;
+    }
+    catch ( std::out_of_range ) {
+        std::cout << "exception for mt[20, 1]=0; catched!\n";
+    }
+    try {
+        mt[-20][-1]=0;
+    }
+    catch ( std::out_of_range ) {
+        std::cout << "exception for mt[-20, -1]=0; catched!\n";
+    }
 
 }
 
