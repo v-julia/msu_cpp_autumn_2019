f5d1b8a9bb580ba8ac57b6a012718ae8b61ce969
v-julia <vjulia94@gmail.com>
2019-11-13 13:31:02 +0300

    fiixed

--- 05/serializer.h
+++ 05/serializer.h
@@ -23,48 +23,38 @@ public:
     }
 
     template <class... ArgsT>
-    Error operator()(ArgsT... args)
+    Error operator()(const ArgsT... args)
     {
         return process(args...);
     }
+
 
 private:
     // process uses variadic templates
     template <class T>
     Error process(T&& val)
     {
-        if (std::is_same<T, uint64_t&>::value) {
-
-            out_ << val << Separator;
-        }
-        else if (std::is_same<T, bool&>::value) {
-
-            out_ << (val ? "true" : "false") << Separator;
-        }
-        else {
-            return Error::CorruptedArchive;
-        }
-        return Error::NoError;
-
+        return print_stream(val);
     }
 
     template <class T, class... Args>
     Error process(T&& val, Args&&... args)
     {
-        if (std::is_same<T, uint64_t&>::value) {
-
-            out_ << val << Separator;
-        }
-        else if (std::is_same<T, bool&>::value) {
-
-            out_ << (val ? "true" : "false") << Separator;
-        }
-        else {
-            return Error::CorruptedArchive;
-        }
-
+        if ( process(val) == Error::CorruptedArchive ) return Error::CorruptedArchive;
         process(std::forward<Args>(args)...);
         return Error::NoError;
+    }
+    
+    Error print_stream(uint64_t val)
+    {
+        out_ << val << Separator;
+        return ( out_ ) ? Error::NoError : Error::CorruptedArchive;
+    }
+
+    Error print_stream(bool val)
+    {
+        out_ << std::boolalpha << val << Separator;
+        return ( out_ ) ? Error::NoError : Error::CorruptedArchive;
     }
 };
 
@@ -75,10 +65,8 @@ class Deserializer
 private:
     std::istream& in_stream_;
     static constexpr char Separator = ' ';
+
 public:
-
-
-
     explicit Deserializer(std::istream& in_a) : in_stream_(in_a) {}
 
     template <class T>
@@ -98,48 +86,29 @@ private:
     template <class T>
     Error process(T&& val)
     {
-        return load(val);
+        return load_value(val);
     }
 
     template <class T, class... Args>
-    Error process(T&& val, Args&... args)
+    Error process(T&& val, Args&&... args)
     {
-        if (load(val) == Error::CorruptedArchive) return Error::CorruptedArchive;
-        process(std::forward<Args&>(args)...);
+        if ( process(val) == Error::CorruptedArchive ) return Error::CorruptedArchive;
+        process(std::forward<Args>(args)...);
         return Error::NoError;
     }
 
-    Error load(uint64_t& value)
+    Error load_value(uint64_t& value)
     {
         in_stream_ >> value;
-
-        if (in_stream_.good()) {
-            return Error::NoError;
-        }
-        else {
-            return Error::CorruptedArchive;
-        }
+        return ( in_stream_ ) ? Error::NoError : Error::CorruptedArchive;
     }
 
-    Error load(bool& value)
+    Error load_value(bool& value)
     {
-        std::string text;
-        in_stream_ >> text;
-
-        if (text == "true")
-            value = true;
-        else if (text == "false")
-            value = false;
-        else
-            return Error::CorruptedArchive;
-
-        return Error::NoError;
+        in_stream_ >> std::boolalpha >> value;
+        return ( in_stream_ ) ? Error::NoError : Error::CorruptedArchive;
     }
-
-
 };
-
-
 
 struct Data
 {
@@ -152,6 +121,8 @@ struct Data
     {
         return serializer(a, b, c);
     }
+
+    template <class Deserializer>
     Error deserialize(Deserializer& deserializer)
     {
         return deserializer(a, b, c);
